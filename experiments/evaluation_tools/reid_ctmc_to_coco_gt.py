f0ad65b559c292e1d0991715658636bc642241d5
somesh khandelia <someshkhandelia@gmail.com>
2022-01-15 13:38:55 +0100

    create seq dir in reid explicitly

--- experiments/evaluation_tools/reid_ctmc_to_coco_gt.py
+++ experiments/evaluation_tools/reid_ctmc_to_coco_gt.py
@@ -97,6 +97,8 @@ def main():
         # generate reid data
         reid_imgs_path = osp.join(data_path, 'reid')
         os.makedirs(reid_imgs_path, exist_ok=True) # create reid directory
+        reid_seq_path = osp.join(reid_imgs_path, seq)
+        os.makedirs(reid_seq_path, exist_ok=True) # create sequence directory inside reid directory
 
         for seq in seqs:
             print(f"Processing sequence {seq} in dataset {args.dataset}")
